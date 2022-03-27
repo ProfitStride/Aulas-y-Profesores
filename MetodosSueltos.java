4ff46a2e358ad0253f5d1d1bf90b5c6d07f46c8d
ProfitStride <102462223+ProfitStride@users.noreply.github.com>
2022-03-27 21:40:23 +0200

    Delete MetodosSueltos.java

--- MetodosSueltos.java
+++
@@ -1,8 +0,0 @@
-package AlumnosProfesores;
-
-public class MetodosSueltos {
-    public static int generaNumeroAleatorio(int minimo, int maximo){
-        int num=(int)Math.floor(Math.random()*(minimo - (maximo+1))+(maximo+1));
-        return num;
-    }
-}
